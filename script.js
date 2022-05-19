2a1f1c4767cb42f92c18bdd22316434469415f50
eniaa <92086184+eniaa@users.noreply.github.com>
2022-05-19 13:16:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-// script.js
-// when user 'clicks' on the div id 'red', evoke the function makeRed()
-document.getElementById(‘red’).onclick = makeRed; 
-function makeRed() {
-document.getElementById('redLight').style.backgroundColor = "red"
+var lightStates = {red:0,amber:1,green:2};
+var currentState = lightStates.red;
+
+document.getElementById('changeBtn').onclick=function(){
+    changeState();
+};
+
+
+function changeState()
+{
+    clear();
+  switch(currentState)
+  {
+    case lightStates.red:
+    {
+      document.getElementById("red").className ="light red";
+      currentState =  lightStates.amber;
+    }
+      break;
+    case lightStates.amber:
+    {
+      document.getElementById("amber").className ="light amber";
+      currentState = lightStates.green;
+    } break;
+     case lightStates.green:
+    {
+      document.getElementById("green").className ="light green";
+      currentState = lightStates.red;
+    } break;
+   }
+}
+
+function clear(){
+   document.getElementById("red").className ="light off";
+   document.getElementById("amber").className ="light off";
+   document.getElementById("green").className ="light off";
 }
